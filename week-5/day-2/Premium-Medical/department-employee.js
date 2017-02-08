93ebda9b24560c1eefb13cbc4be1ff5ed9f5d8d0
jamesgmarks <jamesgmarks@gmail.com>
2017-02-07 23:13:36 -0500

    Added week-5 day-2 starter files

---
+++ week-5/day-2/Premium-Medical/department-employee.js
@@ -0,0 +1,98 @@
+
+const departments = [
+  {
+    code: 'ACCT',
+    name: 'Accounting',
+    budget: 200000,
+  },
+  {
+    code: 'ISYS',
+    name: 'Information Systems',
+    budget: 500000,
+  },
+  {
+    code: 'MKTG',
+    name: 'Marketing',
+    budget: 170000,
+  },
+  {
+    code: 'PERS',
+    name: 'Personnel',
+    budget: 80000,
+  },
+];
+
+const employees = [
+  {
+    firstName: 'Marsha',
+    lastName: 'Bonavoochi',
+    title: 'Gopher',
+    salary: 59000,
+    start: new Date('1991/6/6'),
+    deptCode: 'ACCT',
+  },
+  {
+    firstName: 'Linda',
+    lastName: 'Rootaroni',
+    title: 'Analyst',
+    salary: 75000,
+    start: new Date('1989/10/2'),
+    deptCode: 'ISYS',
+  },
+  {
+    firstName: 'Dave',
+    lastName: 'Kendollini',
+    title: 'Programmer',
+    salary: 47000,
+    start: new Date('1981/11/3'),
+    deptCode: 'ISYS',
+  },
+  {
+    firstName: 'Roberto',
+    lastName: 'Acostaletti',
+    title: 'Manager',
+    salary: 85000,
+    start: new Date('1995/9/6'),
+    deptCode: 'ACCT',
+  },
+  {
+    firstName: 'Dini',
+    lastName: 'Cappelletti',
+    title: 'Admin Assistant',
+    salary: 50500,
+    start: new Date('1990/7/9'),
+    deptCode: 'ACCT',
+  },
+  {
+    firstName: 'Cliffy',
+    lastName: 'Patti',
+    title: 'Technician',
+    salary: 46000,
+    start: new Date('1984/1/1'),
+    deptCode: 'ISYS',
+  },
+  {
+    firstName: 'Peter',
+    lastName: 'Vanscollini',
+    title: 'Manager',
+    salary: 60000,
+    start: new Date('1996/9/10'),
+    deptCode: 'MKTG',
+  },
+  {
+    firstName: 'Davide',
+    lastName: 'Stovellana',
+    title: 'Programmer',
+    salary: 100001,
+    start: new Date('1990/10/9'),
+    deptCode: 'ISYS',
+  },
+  {
+    firstName: 'Jimmi',
+    lastName: 'DeMarco',
+    title: 'Boss Man',
+    salary: 'Nun\'aya Business',
+    start: new Date('1991/6/6'),
+    deptCode: 'ACCT',
+  },
+];
---
+++ week-5/day-2/Premium-Medical/script.js
@@ -0,0 +1,34 @@
+
+// Note: index.html would have loaded department-employee.js first
+  // we are going to use the arrays from that script file just like a database
+
+// 1. Say Hello! (We'll do this together.)
+// Output a welcome message for each employee in our database:
+// Example:
+//     Welcome to the Office Jimmi.
+
+
+// 2. Information Systems Debacle: (Your turn.)
+// For employees that work in the ISYS department (use deptCode)
+// Output their name, title and salary.
+// BONUS: Keep track of total salary and output that at the end.
+
+
+// Aside... While we're at it, lets learn a **bad** way to write conditionals.
+
+
+// 3. Bullets:
+// Using the boilerplate code provided (in-class), change the code 
+// to output a block of data about each of our departments.
+
+// 4. Disfunctions
+
+// 4a.
+// Lets write our first "pure" function.
+// if y = x + 3; Solve for y
+
+
+// 4b.
+// if y = mx + b, write a function that will solve for y given m, x, and b
+
+
