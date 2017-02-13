99021756883711b9bae30b903dfc6e8e67ea877e
jamesgmarks <jamesgmarks@gmail.com>
2017-02-13 06:40:28 -0500

    Added week 6 starters

---
+++ week-6/practice1.js
@@ -0,0 +1,10 @@
+
+var handleSubmit = function() {
+
+	// Have the user enter a month number between 1 and 12.
+	// - First verify that the user entered a valid number. 
+	//   Produce an error message if they did not.
+	// - If they entered a valid number, use a switch statement 
+	//   to output the correct month name. (1 = January, 2 = February, etc...)	
+
+}
---
+++ week-6/practice2.js
@@ -0,0 +1,19 @@
+
+function handleSubmit() {
+	// Expect the user to enter a name in the name text box: 
+	// *** Fit all of the below into one if/else if style structure.
+	// - If the user does not enter a name, 
+	//   > have your application say "Hello World!"
+	// - If the user just enters a number, 
+	//   > have the application greet them and introduce itself as R2D2 or C3PO 
+	//    (your choice) Ex: Hello 42, I'm C3PO! Nice to meet you.
+	// - If the user enters YOUR first name, 
+	//   > have the application respond, "Behold, the Emperess!" 
+	//    (or "Behold, the Emperor" - take your pick)
+	// - If the user enters any other text in the name text box, 
+	//   > trim off any whitespace (look this up on w3schools) 
+	//   > have the application simply output "Hello <their name>!". Ex: Hello James!
+	// ! BONUS ! See if you can figure out how to determine whether the user entered 
+	//           something that would be recognized by Javascript as a date. 
+	//           Give them a different greeting if they did.
+}
