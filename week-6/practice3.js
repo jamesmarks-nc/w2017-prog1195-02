ce03a23f2bd6478e8aa9a7489ea5bae9ec3692d8
jamesgmarks <jamesgmarks@gmail.com>
2017-02-14 22:21:37 -0500

    Added additional practice exercises

---
+++ week-6/practice3.js
@@ -0,0 +1,22 @@
+
+function handleSubmit() {
+
+  // Create an array containing the following 8 Flintstone character names:
+  // For kicks, use the array constructor syntax. (That means, no square brackets!)
+  //  - Fred
+  //  - Wilma
+  //  - Pebbles
+  //  - Barney
+  //  - Betty
+  //  - Bamm-Bamm
+  //  - The Great Gazoo
+  //  - Mr. Slate
+
+  // Get a number between 1 and 8 from the user input textbox.
+  // You'll have to validate their input to be sure they entered
+  // a number that is between 1 and 8
+
+  // Use the user's input number and your knowledge of arrays
+  // to retrieve the name from the list at the appropriate index
+  // and display the name in the output text box.
+}
---
+++ week-6/practice4.js
@@ -0,0 +1,10 @@
+
+function handleSubmit() {
+  // Create an (ordered) array of month names from January to December
+
+  // Have the user enter a month number between 1 and 12.
+  // - First verify that the user entered a number and that it is within the range 1 to 12.
+  //   Produce an error message if they did not.
+  // - If they entered a valid number, use an array method to
+  //   to output the correct month name from the array you created above.
+}
