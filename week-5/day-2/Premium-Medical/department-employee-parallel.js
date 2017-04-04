f4ad9c618ee965a1e629dfacfd451af40fa3f72c
jamesgmarks <jamesgmarks@gmail.com>
2017-04-04 14:29:05 -0400

    Adjusted data from week (5????)

--- week-5/day-2/Premium-Medical/department-employee-parallel.js
+++ week-5/day-2/Premium-Medical/department-employee-parallel.js
@@ -1,15 +1,78 @@
-var firstNames = ["Marsha","Linda","Dave","Roberto","Dini","Cliffy","Peter","Davide","Jimmi"];
-var lastNames = ["Bonavoochi","Rootaroni","Kendollini","Acostaletti","Cappelletti","Patti","Vanscollini","Stovellana","DeMarco"];
-var titles = ["Gopher","Analyst","Programmer","Manager","Admin Assistant","Technician","Manager","Programmer","Boss Man"];
-var salaries = [59000,75000,47000,85000,50500,46000,60000,100001,"Nun'aya Business"];
-var starts = [ new Date("1991-06-06T04:00:00.000Z"),
-new Date("1989-10-02T04:00:00.000Z"),
-new Date("1981-11-03T05:00:00.000Z"),
-new Date("1995-09-06T04:00:00.000Z"),
-new Date("1990-07-09T04:00:00.000Z"),
-new Date("1984-01-01T05:00:00.000Z"),
-new Date("1996-09-10T04:00:00.000Z"),
-new Date("1990-10-09T04:00:00.000Z"),
-new Date("1991-06-06T04:00:00.000Z"),
+var firstNames = [
+  "Marsha",
+  "Linda",
+  "Dave",
+  "Roberto",
+  "Dini",
+  "Cliffy",
+  "Peter",
+  "Davide",
+  "Jimmi"
 ];
-var deptCodes = ["ACCT","ISYS","ISYS","ACCT","ACCT","ISYS","MKTG","ISYS","ACCT"];
+var lastNames = [
+  "Bonavoochi",
+  "Rootaroni",
+  "Kendollini",
+  "Acostaletti",
+  "Cappelletti",
+  "Patti",
+  "Vanscollini",
+  "Stovellana",
+  "DeMarco"
+];
+var titles = [
+  "Gopher",
+  "Analyst",
+  "Programmer",
+  "Manager",
+  "Admin Assistant",
+  "Technician",
+  "Manager",
+  "Programmer",
+  "Boss Man"
+];
+var salaries = [
+  59000,
+  75000,
+  47000,
+  85000,
+  50500,
+  46000,
+  60000,
+  100001,
+  "Nun'aya Business"
+];
+var starts = [ 
+  new Date("1991-06-06T04:00:00.000Z"),
+  new Date("1989-10-02T04:00:00.000Z"),
+  new Date("1981-11-03T05:00:00.000Z"),
+  new Date("1995-09-06T04:00:00.000Z"),
+  new Date("1990-07-09T04:00:00.000Z"),
+  new Date("1984-01-01T05:00:00.000Z"),
+  new Date("1996-09-10T04:00:00.000Z"),
+  new Date("1990-10-09T04:00:00.000Z"),
+  new Date("1991-06-06T04:00:00.000Z"),
+];
+var employeeDeptCodes = [
+  "ACCT",
+  "ISYS",
+  "ISYS",
+  "ACCT",
+  "ACCT",
+  "ISYS",
+  "MKTG",
+  "ISYS",
+  "ACCT"
+];
+var deptCodes = [
+  "ACCT",
+  "ISYS",
+  "MKTG",
+  "PERS",
+]
+var deptNames = [
+  "Accounting",
+  "Information Systems Technology",
+  "Marketing",
+  "Personnel"
+]
