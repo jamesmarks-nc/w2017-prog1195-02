e561b0009b19b9e13b77fad822d30adf175ff3ca
jamesmarks <jamesgmarks@gmail.com>
2017-04-10 10:15:15 -0400

    week 14 day 1 (end of class)

--- week-14/p-man (start)/game.js
+++ week-14/p-man (start)/game.js
@@ -16,7 +16,7 @@ const TEXT_COLOR = "#FFFFFF";
 
 // configuration applicable to level. (should change when a new level is loaded);
 let LEVEL = undefined;
-var mapData = [];
+let mapData = [];
 let spawnRate = 75;
 let gridWidth = 20;
 let gridHeight = 30;
@@ -65,26 +65,40 @@ function drawCircle( x, y, radius, fillStyle ) { // x, y = position of middle of
 }
 
 function drawPlayer( x, y ) { // colors[PLAYER]
+  drawCircle( x, y, 8, colors[PLAYER] );
 }
 function drawEnemy( x, y ) { // colors[ENEMY]
+  drawCircle( x, y, 6, colors[ENEMY] );
 }
 function drawFood( x, y ) { // colors[FOOD]
+  drawCircle( x, y, 2, colors[FOOD] );
 }
 
 
 function loadLevel(level) {
 
   // set LEVEL configuration to level parameter
+  LEVEL = level;
 
-  // set canvas height & width to something appropriate using scale, width and height
+  // set canvas height & width to something appropriate using scale, 
+  // width and height
+  canvas.width = unitScale * level.width;
+  canvas.height = unitScale * level.height;
 
   // copy level configuration into config variables.
-  
+  gridHeight = level.height;
+  gridWidth = level.width;
+  spawnRate = level.spawnRate;
+  player.x = level.playerStart.x;
+  player.y = level.playerStart.y;
   // reset player gameover tracking
+  player.isGameOver = false;
   // reset enemies
+  enemies = [];
   
   // Load map data -> use ES6 spread operator or Object.assign()
-  
+  mapData = [...level.mapData];
+
   // reset buttons
   restartButton.disabled = true;
   continueButton.disabled = true;
@@ -94,15 +108,29 @@ function loadLevel(level) {
 }
 
 function drawBoard() {
-  // loop through each row
-    // loop through each column
-      // get the map data at that square
+  for(var y = 0; y < gridHeight; y++) {// loop through each row
+    for(var x = 0; x < gridWidth; x++) {// loop through each column
+      var data = getSquareData(x, y);
       // and it's color
+      var color = colors[data];
       // draw squares. do special stuff for food.
+      if(data === FOOD) {
+        drawSquare(x, y, colors[NOTHING]);
+        drawFood(x, y);
+      } else {
+        drawSquare(x, y, color);
+      }
+    }
+  }
 
   // draw the player
+  drawPlayer(player.x, player.y);
 
   // draw the enemies
+  for(var i = 0; i < enemies.length; i++ ) {
+    var current = enemies[i];
+    drawEnemy(current.x, current.y);
+  }
 }
 
 // getter and setter for what is stored in a given square in map data
@@ -115,10 +143,31 @@ function setSquareData(x, y, newValue) {
 
 function tryMovePlayer(direction) {
   // start with next square where the player is
+  var nextSquare = {
+    x: player.x,
+    y: player.y
+  };
   // then based on the direction we want to go, offset next square
-  
+  switch(direction) {
+    case "L":
+      nextSquare.x = nextSquare.x - 1;
+      break;
+    case "R":
+      nextSquare.x = nextSquare.x + 1;
+      break;
+    case "U":
+      nextSquare.y = nextSquare.y - 1;
+      break;
+    case "D":
+      nextSquare.y = nextSquare.y + 1;
+      break;
+  }
   // figure out the value at that square
+  var nextSquareData = getSquareData(nextSquare.x, nextSquare.y);
 
+  if(nextSquareData === NOTHING || nextSquareData === FOOD) {
+    movePlayer(nextSquare);
+  }
   // if there's an enemy, tough love - game over
   // else if it's a good square (food or nothing), then move there.
   // else if its a wall, don't change position, but trigger a move enemies anyhow (to get "bump" effect)
@@ -127,13 +176,34 @@ function tryMovePlayer(direction) {
 
 function movePlayer(position) { // position must have an x and a y property
   // assume the given position is okay, set player to given position
-  
+  player.x = position.x;
+  player.y = position.y;
   // if it's food, consume it (and update score) then do a win check
+  if(getSquareData(player.x, player.y) === FOOD) {
+    updatePlayerScore(player.score + 10); // Points for eating 
+    setSquareData(player.x, player.y, NOTHING);
+  }
 
   // move all the enemies
+  for(var i = 0; i < enemies.length; i++) {
+    moveEnemy(i);
+  }
 
   // manage spawn points. (use spawnRate)
   // Note: just have to add enemy to the enemies array and the board will include them next draw.
+  for(var i = 0; i < mapData.length; i++) {
+    if(mapData[i] === SPAWNER) {
+      var doSpawn = Math.floor(Math.random() * spawnRate) + 1;
+      if(doSpawn === spawnRate) {
+        enemies.push(
+          {
+            x: i % gridWidth,
+            y: Math.floor(i / gridWidth)
+          }
+        )
+      }
+    }
+  }
 
   // if game is not over, draw the board.
   if(!player.isGameOver) {
@@ -208,13 +278,38 @@ function moveEnemy(enemyIndex) {
   // Check the next square until the next square is on the "nice list".
   do {
     // Initialize our next square to the enemy's current position.
-    
+    nextSquare = {
+      x: enemy.x,
+      y: enemy.y
+    };
     // Check enemy.d (direction) and update next square object accordingly
-
+    switch(enemy.d) {
+      case "L":
+        nextSquare.x -= 1;
+        break;
+      case "R":
+        nextSquare.x += 1;
+        break;
+      case "U":
+        nextSquare.y -= 1;
+        break;
+      case "D":
+        nextSquare.y += 1;
+        break;
+    }
     // get the data at that square
+    var data = getSquareData(nextSquare.x, nextSquare.y);
     // make sure the enemy is allowed to move there
-    // if not, randomize which direction the enemy will go next
-    // if the player is there, it's game over time.
+    if(data !== FOOD && data !== NOTHING) {
+      // if not, randomize which direction the enemy will go next
+      var directions = getAvailableDirections(enemy.x, enemy.y);
+      console.log(directions);
+      var index = Math.floor(Math.random() * directions.length);
+      enemy.d = directions[index];
+    } else if(playerIsAt(nextSquare.x, nextSquare.y)) {
+      // if the player is there, it's game over time.
+      gameOver("Enemy ate player");
+    }
     
   } while(nextSquareData !== NOTHING && nextSquareData !== FOOD); // Until its a square the enemy is allowed to walk on.
 
@@ -252,8 +347,22 @@ function getAvailableDirections(x, y) {
 
 // capture when a player presses a key on your screen and start a turn.
 document.onkeydown = function(e) {
-  // Make sure the game is not over and then figure out which key is pressed then tryMovePlayer() in the appropriate direction.
-  // Note: you can use console.log to figure out which key you're pressing and map them any way you want to.
+  if(!player.isGameOver) {
+    switch(e.keyCode) {
+      case 37: // L
+        tryMovePlayer("L");
+        break;
+      case 38: // U
+        tryMovePlayer("U");
+        break;
+      case 39: // R
+        tryMovePlayer("R");
+        break;
+      case 40: // D
+        tryMovePlayer("D");
+        break;
+    }
+  }
 }
 
 // html button even handlers.
@@ -261,3 +370,4 @@ restartButton.onclick = restart;
 continueButton.onclick = nextLevel;
 
 // load the first level >>> loadLevel()
+loadLevel(level0);
